ebef761b2e2edac0613fc353df9346cf646c1fa8
isotocchs <89474695+isotocchs@users.noreply.github.com>
2022-10-13 21:21:30 -0400

    save

--- testing.js
+++ testing.js
@@ -1,3 +1,109 @@
-console.log("Hello World");
-console.log(true);
-console.log(123);
+// console.log("Hello World");
+// console.log(true);
+// console.log(123);
+
+// //var = variable -  you can change it
+
+// var someName = "John Ham";
+// console.log(someName);
+// someName = "Tim Apple";
+// console.log(someName);
+
+// //initialize a variable
+// var temp;
+// console.log(temp);
+
+// temp = 45;
+// console.log(temp);
+
+// //the name of a variable can have letter, numbers, _ or $
+// //but it cannot start with a number
+
+// //
+
+// //let - variables that are limited in scope
+// // works the same as var
+// let somethingNew;
+// let newName = "Sam Johnson";
+// console.log(newName);
+// newName = "Something Else";
+// console.log(newName);
+
+// //const = constant, cannot be reassigned later
+// const theBest = "Michael Jordan";
+// console.log(theBest);
+// // theBest = "LeBron James";
+// // console.log(theBest);
+
+// // const must also be initialized with some value, can't just be declared
+// const newThing;
+
+// Data Types
+
+// Primitive Types - stored directly on the location the variable accesses
+
+// String - Words - always in quotes "Apples"
+// Numbers - 1 -2 3 4 5.45
+// Boolean - true or false
+// Null - empty value
+// Undefined - variable that has not been assined info
+// Symbols -
+
+// Reference Types - info is stored elsewhere. Pointer to another location
+// Arrays - lists of things
+// Object literals
+// Functions
+// Dates
+// Etc.
+
+// //Strings
+// const colors = "blue";
+// console.log(typeof colors);
+
+// //numbers
+// const age = 34;
+// console.log(typeof age);
+
+// //booleans
+// const hasCar = true;
+// console.log(typeof hasCar);
+
+// // null
+// const carType = null;
+// console.log(typeof carType);
+
+// //undefined
+// let testing;
+// console.log(typeof testing);
+
+// //symbol
+// const symb = Symbol();
+// console.log(typeof symb);
+
+// Reference Types - objects
+
+// //Arrays - List of things
+
+// const dogBreeds = ["Geraman Shepherd", "Golden Retreiver", "Chihuahua", 45, 23];
+// console.log(typeof dogBreeds);
+// console.log(dogBreeds);
+// console.log(dogBreeds[1]);
+// console.log(typeof dogBreeds[1]);
+// console.log(typeof dogBreeds[3]);
+
+// //Object Literals
+// const person = {
+//   name: "John Smith",
+//   age: 23,
+//   height: 6.4,
+// };
+
+// console.log(typeof person);
+// console.log(person);
+// console.log(person.name);
+// console.log(typeof person.name);
+
+// //Date
+// const today = new Date();
+// console.log(typeof today);
+// console.log(today);
