24b1ebc05ddf51e287c03a90e083bbc22af0ff24
isotocchs <89474695+isotocchs@users.noreply.github.com>
2022-10-31 20:55:53 -0400

    save

--- canvasJS.js
+++ canvasJS.js
@@ -1,27 +1,56 @@
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
-console.log(ctx);
+const bob = document.getElementById("canvas1");
+// console.log(bob);
+const bobContext = bob.getContext("2d");
+// console.log(bobContext);
 
-const canvas_width = (canvas.width = 600);
-const canvas_height = (canvas.height = 600);
+const canvas_width = (bob.width = 600);
+const canvas_height = (bob.height = 600);
 
-// function animate() {
-//   ctx.clearRect(0, 0, canvas_width, canvas_height);
-//   ctx.fillRect(x, 50, 100, 100);
-//   x++;
-//   requestAnimationFrame(animate);
-// }
+function animate() {
+  bobContext.clearRect(0, 0, canvas_width, canvas_height);
+  bobContext.fillRect(50, tom, 100, 100);
+  tom++;
+  requestAnimationFrame(animate);
+}
 
-// let x = 0;
+let tom = 0;
 // animate();
 
-// const playerImage = new Image();
-// playerImage.src = "bat-sprite.png";
+const playerImage = new Image();
+playerImage.src = "bat-sprite.png";
 
-// function animate2() {
-//   ctx.clearRect(0, 0, canvas_width, canvas_height);
-//   //   ctx.fillRect(50, 50, 100, 100);
-//   ctx.drawImage(playerImage, 0, 0);
-//   requestAnimationFrame(animate2);
-// }
+const spriteWidth = 128 / 4;
+const spriteHeight = 128 / 4;
+let frameX = 1;
+let frameY = 1;
+let gameFrame = 0;
+const stagerFramesBy = 10;
+
+function animate2() {
+  bobContext.clearRect(0, 0, canvas_width, canvas_height);
+  //bobContext.fillRect(50, 50, 100, 100);
+  //bobContext.drawImage(playerImage, source_x, source_y, source_w, source_h, destination_x,destination_y,destination_w,destination_h);
+  bobContext.drawImage(
+    playerImage,
+    frameX * spriteWidth,
+    frameY * spriteHeight,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    canvas_width,
+    canvas_height
+  );
+  // if (gameFrame % stagerFramesBy == 0) {
+  //   if (frameX < 3) {
+  //     frameX++;
+  //   } else {
+  //     frameX = 1;
+  //   }
+  // }
+
+  // gameFrame++;
+  requestAnimationFrame(animate2);
+}
+
 // animate2();
