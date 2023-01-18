3c10e36f84d701280b210d519bb857c521efe7fa
isotocchs <89474695+isotocchs@users.noreply.github.com>
2023-01-18 08:50:38 -0500

    save

--- canvasEnemy.js
+++ canvasEnemy.js
@@ -10,8 +10,8 @@ const canvas_width = (bob.width = 600);
 const canvas_height = (bob.height = 600);
 
 // lets make many enemies
-// const numberOfEnemies = 10;
-// const enemiesArray = [];
+const numberOfEnemies = 100;
+const enemiesList = [];
 
 // let enemyx = 10;
 // let enemyy = 50;
@@ -35,15 +35,18 @@ const canvas_height = (bob.height = 600);
 // };
 
 // // console.log(desk);
+console.log(Math.random());
 
 // create enemy class - to produce multiple enemies
 class Enemy {
   constructor() {
     //this keyword refers to the new object you are creating
-    this.x = 10;
-    this.y = 50;
-    this.width = 100;
-    this.height = 100;
+    this.x = Math.random() * canvas_width;
+    this.y = Math.random() * canvas_height;
+    this.width = 50;
+    this.height = 50;
+    this.speed = Math.random() * 4 - 2;
+
     //lets make the initial position random instead
     // this.x = Math.random() * canvas_width;
     // this.y = Math.random() * canvas_height;
@@ -54,13 +57,13 @@ class Enemy {
   //classes can have custom methods.
   //Lets make one that changes the position of the enemies
   //instead of doing it by hand.
-  update() {
-    this.x += 0.5;
-    this.y += 1;
+  updateMovement() {
+    // this.x += 0.5;
+    // this.y += 1;
 
     // lets use the speed to change the direction
     // this.x += this.speed;
-    // this.y += this.speed;
+    this.y += this.speed;
   }
   //lets also make a draw method that lets us draw enemies easier.
   drawEnemy() {
@@ -71,14 +74,15 @@ class Enemy {
 }
 
 //lets make an enemy object
-const enemyObjectFromClass = new Enemy();
+// const enemyObjectFromClass = new Enemy();
+// console.log(enemyObjectFromClass);
 
 // lets make many enemies
-// for (let i = 0; i < numberOfEnemies; i++) {
-//   //push method takes whatever you give it and
-//   //pushes it to the end of the array
-//   enemiesArray.push(new Enemy());
-// }
+for (let i = 0; i < numberOfEnemies; i++) {
+  //push method takes whatever you give it and
+  //pushes it to the end of the array
+  enemiesList.push(new Enemy());
+}
 //use console.log to make sure that your loop and everything else is working
 // console.log(enemiesArray);
 
@@ -87,24 +91,26 @@ function animate() {
   bobContext.clearRect(0, 0, canvas_width, canvas_height);
 
   //move enemy in x axis
-  enemy1.x += 0.5;
+  // enemy1.x += 0.5;
+  // enemyObjectFromClass.updateMovement();
 
   //move enemy in y axis
-  enemy1.y += 1;
+  // enemy1.y += 1;
   //move enemy with update method instead. make sure to end it with () when you call it.
   //enemy1.update();
 
   //draw the enemy rect
-  bobContext.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+  // bobContext.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
 
   //draw enemy with the draw method instead.
-  //enemy1.draw();
+  // enemyObjectFromClass.drawEnemy();
 
   //use the forEach method to update and draw each enemy in the array.
-  //   enemiesArray.forEach((enemy) => {
-  //     enemy.update();
-  //     enemy.draw();
-  //   });
+  enemiesList.forEach((enemy) => {
+    enemy.updateMovement();
+    enemy.drawEnemy();
+  });
+
   //create the infinate loop
   requestAnimationFrame(animate);
 }
