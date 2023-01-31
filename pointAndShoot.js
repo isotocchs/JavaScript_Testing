7701bdce4819d1c6a9813535fb2e334888ebd897
isotocchs <89474695+isotocchs@users.noreply.github.com>
2023-01-31 08:07:31 -0500

    save

--- pointAndShoot.js
+++ pointAndShoot.js
@@ -10,72 +10,73 @@ const canvas_height = (bob.height = 600);
 let canvasPosition = bob.getBoundingClientRect();
 const explosions = [];
 
-class Explosion {
-  //now constructor takes in Info - (x,y) coordinates
-  constructor(x, y) {
-    this.expMaxFramesX = 8;
-    this.expMaxFramesY = 6;
-    this.expWidth = 1920 / this.expMaxFramesX;
-    this.expHeight = 1440 / this.expMaxFramesY;
-    this.drawWidth = this.expWidth * 0.7;
-    this.drawHeight = this.expHeight * 0.7;
-    this.expX = x - this.drawWidth / 2;
-    this.expY = y - this.drawHeight / 2;
-
-    //new way to import the image
-    this.expImage = new Image();
-    this.expImage.src = "explosions.png";
-    this.expFrameX = 0;
-    this.expFrameY = 0;
-    this.gameFrame = 0;
-  }
-  update() {
-    this.gameFrame++;
-    if (this.gameFrame % 2 === 0) {
-      if (this.expFrameX < this.expMaxFramesX - 1) {
-        this.expFrameX++;
-      } else {
-        this.expFrameY++;
-        this.expFrameX = 0;
-      }
-    }
-  }
-
-  draw() {
-    bobContext.drawImage(
-      this.expImage,
-      this.expWidth * this.expFrameX,
-      this.expHeight * this.expFrameY,
-      this.expWidth,
-      this.expHeight,
-      this.expX,
-      this.expY,
-      this.drawWidth,
-      this.drawHeight
-    );
-  }
-}
-
 //lets listen for a mouse click
 window.addEventListener("click", function (e) {
   let testWidth = 50;
   let testHeight = 50;
-  let positionX = e.x - canvasPosition.left - testWidth / 2;
-  let positionY = e.y - canvasPosition.top - testHeight / 2;
+  console.log(e);
+  //   let positionX = e.x - canvasPosition.left - testWidth / 2;
+  //   let positionY = e.y - canvasPosition.top - testHeight / 2;
 
-  //   bobContext.fillRect(positionX, positionY, testWidth, testHeight);
-  let expPositionX = e.x - canvasPosition.left;
-  let expPositionY = e.y - canvasPosition.top;
+  bobContext.fillRect(e.x, e.y, testWidth, testHeight);
+  //   let expPositionX = e.x - canvasPosition.left;
+  //   let expPositionY = e.y - canvasPosition.top;
 
-  explosions.push(new Explosion(expPositionX, expPositionY));
+  //   explosions.push(new Explosion(expPositionX, expPositionY));
 });
 
-function animate() {
-  bobContext.clearRect(0, 0, canvas_width, canvas_height);
-  for (let i = 0; i < explosions.length; i++) {
-    explosions[i].update();
-    explosions[i].draw();
-  }
-  requestAnimationFrame(animate);
-}
-animate();
+// class Explosion {
+//   //now constructor takes in Info - (x,y) coordinates
+//   constructor(x, y) {
+//     this.expMaxFramesX = 8;
+//     this.expMaxFramesY = 6;
+//     this.expWidth = 1920 / this.expMaxFramesX;
+//     this.expHeight = 1440 / this.expMaxFramesY;
+//     this.drawWidth = this.expWidth * 0.7;
+//     this.drawHeight = this.expHeight * 0.7;
+//     this.expX = x - this.drawWidth / 2;
+//     this.expY = y - this.drawHeight / 2;
+
+//     //new way to import the image
+//     this.expImage = new Image();
+//     this.expImage.src = "explosions.png";
+//     this.expFrameX = 0;
+//     this.expFrameY = 0;
+//     this.gameFrame = 0;
+//   }
+//   update() {
+//     this.gameFrame++;
+//     if (this.gameFrame % 2 === 0) {
+//       if (this.expFrameX < this.expMaxFramesX - 1) {
+//         this.expFrameX++;
+//       } else {
+//         this.expFrameY++;
+//         this.expFrameX = 0;
+//       }
+//     }
+//   }
+
+//   draw() {
+//     bobContext.drawImage(
+//       this.expImage,
+//       this.expWidth * this.expFrameX,
+//       this.expHeight * this.expFrameY,
+//       this.expWidth,
+//       this.expHeight,
+//       this.expX,
+//       this.expY,
+//       this.drawWidth,
+//       this.drawHeight
+//     );
+//   }
+// }
+
+// function animate() {
+//   bobContext.clearRect(0, 0, canvas_width, canvas_height);
+//   for (let i = 0; i < explosions.length; i++) {
+//     explosions[i].update();
+//     explosions[i].draw();
+//   }
+//   requestAnimationFrame(animate);
+// }
+// animate();
